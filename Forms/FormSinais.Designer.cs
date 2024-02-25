8b6b17e0b4b1c3565c045d9d2f22f050f9784cf6
CarlosMSVilaca <155680516+CarlosMSVilaca@users.noreply.github.com>
2024-02-25 04:52:21 +0000

    alterar X em textbox

--- Forms/FormSinais.Designer.cs
+++ Forms/FormSinais.Designer.cs
@@ -51,15 +51,14 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(340, 172);
-            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Location = new System.Drawing.Point(255, 140);
             this.chart1.Name = "chart1";
             this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(628, 369);
+            this.chart1.Size = new System.Drawing.Size(471, 300);
             this.chart1.TabIndex = 15;
             this.chart1.Text = "chart1";
             // 
@@ -68,10 +67,9 @@
             this.txtBoxFreq.BackColor = System.Drawing.Color.LightGray;
             this.txtBoxFreq.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtBoxFreq.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxFreq.Location = new System.Drawing.Point(158, 245);
-            this.txtBoxFreq.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBoxFreq.Location = new System.Drawing.Point(128, 199);
             this.txtBoxFreq.Name = "txtBoxFreq";
-            this.txtBoxFreq.Size = new System.Drawing.Size(132, 21);
+            this.txtBoxFreq.Size = new System.Drawing.Size(99, 17);
             this.txtBoxFreq.TabIndex = 14;
             // 
             // txtBoxVolt
@@ -79,20 +77,18 @@
             this.txtBoxVolt.BackColor = System.Drawing.Color.LightGray;
             this.txtBoxVolt.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtBoxVolt.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxVolt.Location = new System.Drawing.Point(158, 195);
-            this.txtBoxVolt.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBoxVolt.Location = new System.Drawing.Point(127, 158);
             this.txtBoxVolt.Name = "txtBoxVolt";
-            this.txtBoxVolt.Size = new System.Drawing.Size(133, 21);
+            this.txtBoxVolt.Size = new System.Drawing.Size(100, 17);
             this.txtBoxVolt.TabIndex = 13;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(16, 246);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(12, 200);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 21);
+            this.label3.Size = new System.Drawing.Size(68, 15);
             this.label3.TabIndex = 12;
             this.label3.Text = "Frequência";
             // 
@@ -100,10 +96,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(16, 196);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(12, 159);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 21);
+            this.label2.Size = new System.Drawing.Size(45, 15);
             this.label2.TabIndex = 11;
             this.label2.Text = "Tensão";
             // 
@@ -112,10 +107,9 @@
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(585, 49);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(439, 40);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(159, 50);
+            this.button3.Size = new System.Drawing.Size(119, 41);
             this.button3.TabIndex = 10;
             this.button3.Text = "Onda Triangular";
             this.button3.UseVisualStyleBackColor = true;
@@ -126,10 +120,9 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(340, 49);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(255, 40);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(159, 50);
+            this.button2.Size = new System.Drawing.Size(119, 41);
             this.button2.TabIndex = 9;
             this.button2.Text = "Onda Quadrada";
             this.button2.UseVisualStyleBackColor = true;
@@ -140,10 +133,9 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(93, 49);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(70, 40);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 50);
+            this.button1.Size = new System.Drawing.Size(119, 41);
             this.button1.TabIndex = 8;
             this.button1.Text = "Onda Sinusoidal";
             this.button1.UseVisualStyleBackColor = true;
@@ -151,9 +143,9 @@
             // 
             // FormSinais
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.txtBoxFreq);
             this.Controls.Add(this.txtBoxVolt);
@@ -162,8 +154,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.MinimumSize = new System.Drawing.Size(898, 446);
+            this.MinimumSize = new System.Drawing.Size(678, 370);
             this.Name = "FormSinais";
             this.Text = "Sinais Digitais";
             this.Load += new System.EventHandler(this.FormSinais_Load);
